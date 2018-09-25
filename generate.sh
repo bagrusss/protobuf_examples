f6eae3a9632a31f627b116bc5c5ce10cd11d4a8f
Vladislav Kozhushko <bagrusss@gmail.com>
2018-09-25 16:43:01 +0300

    delete old proto fils before new generation

--- generate.sh
+++ generate.sh
@@ -4,4 +4,5 @@ PROTO_FILES=`find ./proto/ -name \*.proto -print`
 PROTOBUF="./proto"
 PROTOMODEL='./src/main/java'
 
+rm -rf PROTOMODEL
 protoc --proto_path=${PROTOBUF}/ --java_out=${PROTOMODEL} ${PROTO_FILES}
