177357f30aa1d1a141496d23083e259ea62fd819
Vladislav Kozhushko <bagrusss@gmail.com>
2018-09-26 12:41:22 +0300

    json utils

---
+++ src/main/kotlin/ru/bagrusss/demo/ProtoJsonUtils.kt
@@ -0,0 +1,23 @@
+package ru.bagrusss.demo
+
+import com.google.protobuf.Message
+import com.google.protobuf.util.JsonFormat
+
+object ProtoJsonUtils {
+
+    private val jsonParser = JsonFormat.parser()
+    private val jsonPrinter = JsonFormat.printer()
+
+    fun <M : Message, B : Message.Builder> fromJson(json: String, protoBuilder: B): M {
+        return protoBuilder.run {
+            jsonParser.merge(json, this)
+            build() as M
+        }
+    }
+
+    fun <M : Message> toJson(proto: M): String {
+        return jsonPrinter.print(proto)
+    }
+
+}
+
